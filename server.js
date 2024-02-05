9df9c1e306065f07cd856fec425ca9a12a170e10
Biswajeet Jena <93365325+66biswajeet@users.noreply.github.com>
2024-02-05 10:02:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const clientsecret = "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";
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
